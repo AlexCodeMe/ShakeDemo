82ef0a2306a01a286fcc49297e44ee39879e572c
alex moyer <143504417+chloegome@users.noreply.github.com>
2023-12-16 07:04:51 -0500

    added GraphView to record accelerometer data

--- app/src/main/java/com/example/shakedemo/MainActivity.java
+++ app/src/main/java/com/example/shakedemo/MainActivity.java
@@ -11,6 +11,11 @@ import android.os.Bundle;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.Viewport;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+
 public class MainActivity extends AppCompatActivity {
 
     TextView txt_currentAccel, txt_prevAccel, txt_acceleration;
@@ -21,6 +26,17 @@ public class MainActivity extends AppCompatActivity {
     private Sensor mAccelerometer;
     private double accelerationCurrentValue;
     private double accelerationPreviousValue;
+
+    private int pointsPlotted = 5;
+    private int graphIntervalCounter = 0;
+    private Viewport viewport;
+    LineGraphSeries<DataPoint> series = new LineGraphSeries<DataPoint>(new DataPoint[] {
+            new DataPoint(0, 1),
+            new DataPoint(1, 5),
+            new DataPoint(2, 3),
+            new DataPoint(3, 2),
+            new DataPoint(4, 6)
+    });
     private SensorEventListener sensorEventListener = new SensorEventListener() {
         @Override
         public void onSensorChanged(SensorEvent sensorEvent) {
@@ -49,7 +65,16 @@ public class MainActivity extends AppCompatActivity {
             } else {
                 txt_acceleration.setBackgroundColor(getResources().getColor(com.google.android.material.R.color.design_default_color_background));
             }
+            pointsPlotted++;
+            if (pointsPlotted > 1000) {
+                pointsPlotted = 1;
+                series.resetData(new DataPoint[] { new DataPoint(1, 0)});
+            }
+            series.appendData(new DataPoint(pointsPlotted, changeInAcceleration), true, pointsPlotted);
+            viewport.setMaxX(pointsPlotted);
+            viewport.setMinX(pointsPlotted - 200);
         }
+
 
 
 
@@ -72,6 +97,14 @@ public class MainActivity extends AppCompatActivity {
 
         mSensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
         mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+        // sample graph code
+
+        GraphView graph = (GraphView) findViewById(R.id.graph);
+        viewport = graph.getViewport();
+        viewport.setScrollable(true);
+        viewport.setXAxisBoundsManual(true);
+        graph.addSeries(series);
     }
 
     protected void onResume() {
